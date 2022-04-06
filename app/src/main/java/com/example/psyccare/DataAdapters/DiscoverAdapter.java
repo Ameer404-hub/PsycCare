e8757c4862d31aebeda26c0d55496134c162c0c4
Ameer404 <79346541+Ameer404-hub@users.noreply.github.com>
2022-04-07 00:29:59 +0500

    Discover Activity Implementation
    Implemented front end & bank end of discover activity; card view, recycler view, data adapters.

---
+++ app/src/main/java/com/example/psyccare/DataAdapters/DiscoverAdapter.java
@@ -0,0 +1,65 @@
+package com.example.psyccare.DataAdapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.psyccare.DataModels.DiscoverDataModel;
+import com.example.psyccare.R;
+
+import java.util.ArrayList;
+
+public class DiscoverAdapter extends RecyclerView.Adapter<DiscoverAdapter.DiscoverViewHolder> {
+
+    Context context;
+    ArrayList<DiscoverDataModel> discoverData;
+
+    public DiscoverAdapter(Context context, ArrayList<DiscoverDataModel> discoverData) {
+        this.context = context;
+        this.discoverData = discoverData;
+    }
+
+    @NonNull
+    @Override
+    public DiscoverViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.discover_card_design,parent,false);
+        DiscoverViewHolder discoverViewHolder = new DiscoverViewHolder(view);
+        Animation animation = AnimationUtils.loadAnimation(context, R.anim.fui_slide_in_right);
+        view.startAnimation(animation);
+        return discoverViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DiscoverViewHolder holder, int position) {
+        DiscoverDataModel items = discoverData.get(position);
+        holder.title.setText(items.getTitle());
+        Glide.with(context).load(discoverData.get(position).getImageUri()).into(holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return discoverData.size();
+    }
+
+    public static class DiscoverViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView image;
+        TextView title;
+
+        public DiscoverViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.cardImage);
+            title = itemView.findViewById(R.id.cardText);
+        }
+    }
+}
--- app/src/main/java/com/example/psyccare/Fragments/DiscoverFragment.java
+++ app/src/main/java/com/example/psyccare/Fragments/DiscoverFragment.java
@@ -1,22 +1,89 @@
 package com.example.psyccare.Fragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Toast;
 
+import com.example.psyccare.DataAdapters.DiscoverAdapter;
+import com.example.psyccare.DataModels.DiscoverDataModel;
 import com.example.psyccare.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class DiscoverFragment extends Fragment {
+
+    RecyclerView viewIntroduction, viewFeatured;
+    DatabaseReference referenceToDiscover;
+    DiscoverAdapter adapter;
+    ArrayList<DiscoverDataModel> discoverData;
+    ProgressDialog messageBox;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_discover, container, false);
+
+        viewIntroduction = rootView.findViewById(R.id.discoverView);
+        viewFeatured = rootView.findViewById(R.id.discoverView2);
+        referenceToDiscover = FirebaseDatabase.getInstance().getReference("DiscoverData");
+
+        messageBox = new ProgressDialog(getActivity());
+        messageBox.setTitle("");
+        messageBox.setMessage("Loading...");
+
+        discoverData = new ArrayList<>();
+        adapter = new DiscoverAdapter(getActivity(), discoverData);
+
+        viewIntroduction.setHasFixedSize(true);
+        viewFeatured.setHasFixedSize(true);
+        viewIntroduction.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        viewFeatured.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        viewIntroduction.setAdapter(adapter);
+        viewFeatured.setAdapter(adapter);
+
+        messageBox.show();
+        getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        referenceToDiscover.child("Introduction").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for(DataSnapshot ds : snapshot.getChildren()){
+                        DiscoverDataModel newModel = ds.getValue(DiscoverDataModel.class);
+                        discoverData.add(newModel);
+                    }
+                }
+                else{
+                    messageBox.dismiss();
+                    getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                    Toast.makeText(getActivity(), "Snapshot does not exist", Toast.LENGTH_SHORT).show();
+                }
+                messageBox.dismiss();
+                getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return  rootView;
     }
